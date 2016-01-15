f5fa5beca225a5714694829699a7c23793e92201
syedkaleem29 <kaleem105@gmail.com>
2016-01-15 18:54:14 +0530

    Update puzzle.c

--- Documents/puzzle.c
+++ Documents/puzzle.c
@@ -74,19 +74,19 @@ int checkCompletion(int array[][4])
 {
 	int i,j;
 	int num=1;
-	int flag=1;
+	int flag=0;
 	for(i=0;i<4;i++)
 	{
 		for(j=0;j<4;j++)
 		{
 			if(array[i][j]==num && array[3][3]==0)
 			{
-				flag++;
+				flag=1;
 				num++;
 			}
 		}
 	}
-	if(flag==16)
+	if(flag==1)
 	{
 		return 1;
 	}
