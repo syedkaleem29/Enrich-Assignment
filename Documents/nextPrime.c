51cabbc58a79fd44507deea288c488edcff573c1
syedkaleem29 <kaleem105@gmail.com>
2016-02-01 01:02:41 +0530

    Next Prime

--- Documents/nextPrime.c
+++ Documents/nextPrime.c
@@ -10,39 +10,34 @@ void main()
 }
 int nextPrime(int num)
 {
-<<<<<<< HEAD
-	int i,j,flag;
-	if(num==1|| num==0)
-=======
+	#include<stdio.h>
+#include<math.h>
+int nextPrime(int num);
+void main()
+{
+	int num,prime;
+	scanf("%d",&num);
+	prime=nextPrime(num);
+	printf("The Next Prime Number is %d",prime);
+}
+int nextPrime(int num)
+{
 	int j=3;
 	if(num<2)
->>>>>>> origin/master
 	{
 		return 2;
 	}
 	num=(num%2)==0?num+1:num+2;
-	for(i=num;;i=i+2)
+	while(j<=sqrt(num))
 	{
-		flag=1;
-		for(j=2;j<=sqrt(num);j++)
+		if(num%j==0)
 		{
-<<<<<<< HEAD
-			if(i%j==0)
-			{
-				flag=0;
-			}
-		}
-		if(flag==1)
-		{
-			return i;		
-		}
-=======
 			num=num+2;
 			j=3;
 			continue;
 		}
 		j=j+2;
->>>>>>> origin/master
 	}
 	return num;
+}
 }
